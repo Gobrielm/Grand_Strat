e8ad435e619cdf620711915b6030f7bb2cc2ab88
Gabriel McKay <gabriel.p.mckay@gmail.com>
2025-01-01 13:47:53 -0800

    cleaned files

--- rail_vertex_endpoint.gd
+++
@@ -1,9 +0,0 @@
-class_name rail_vertex_endpoint extends rail_vertex
-
-func _init(endpoint: rail_vertex):
-	location = endpoint.location
-	connections = endpoint.connections
-	for vertex: rail_vertex in connections:
-		vertex.add_connection(self, vertex.get_length(endpoint))
-		vertex.remove_connection(endpoint)
-	endpoint.queue_free()
